ebeac401a6f9eb50ef3ddbc62c61633b3cd4abd9
LionVas <lionbird@mail.ru>
2025-12-12 11:23:25 -0500

    final commit

--- posts.php
+++ posts.php
@@ -16,9 +16,10 @@ $main_text = $rows['main_text'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Васильев Леонид</title>
-    <link rel="stylesheet" href="styles/styles.css">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
     <div class = "container d-flex justify-content-center align-items-center vh-100">
